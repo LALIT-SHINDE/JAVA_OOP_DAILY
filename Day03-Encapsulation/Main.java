60a0b2de3696b02adc842905fb61a4d33e1a01b5
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-24 21:20:32 +0530

    Update Main.java

--- Day03-Encapsulation/Main.java
+++ Day03-Encapsulation/Main.java
@@ -1,1 +1,34 @@
+class Student {
+    private String name;
+    private int age;
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if(age > 0) {
+            this.age = age;
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        s.setName("Siddharth");
+        s.setAge(20);
+
+        System.out.println("Name: " + s.getName());
+        System.out.println("Age: " + s.getAge());
+    }
+}
