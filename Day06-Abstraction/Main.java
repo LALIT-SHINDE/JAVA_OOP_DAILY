4c87dd020d1d6aa5a88b3777527dc057f81f3a9c
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-27 21:36:35 +0530

    Update Main.java

--- Day06-Abstraction/Main.java
+++ Day06-Abstraction/Main.java
@@ -1,1 +1,28 @@
+// 1 — Abstract Class
+abstract class Animal {
+    abstract void sound();   
 
+    void sleep() {           
+        System.out.println("Sleeping...");
+    }
+}
+
+class Dog extends Animal {
+    void sound() {
+        System.out.println("Barking...");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog();
+
+        d.sound();
+        d.sleep();
+    }
+}
+/*
+output:
+Barking...
+Sleeping...
+*/
