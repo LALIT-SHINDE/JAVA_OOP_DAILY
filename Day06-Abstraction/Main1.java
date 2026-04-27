f7e3ff56fd8c9afafa2f252e72163d89f92576de
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-27 21:39:13 +0530

    Update Main1.java

--- Day06-Abstraction/Main1.java
+++ Day06-Abstraction/Main1.java
@@ -1,1 +1,21 @@
+// 2 — Interface
+interface Vehicle {
+    void start();
+}
 
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car starts with key");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.start();
+    }
+}
+/*
+Output:
+Car starts with key
+*/
