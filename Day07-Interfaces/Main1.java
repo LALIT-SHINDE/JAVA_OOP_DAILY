b47be8d2f4fae64409eb5d92f7aaed43f7a64a66
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-28 21:53:07 +0530

    Update Main1.java

--- Day07-Interfaces/Main1.java
+++ Day07-Interfaces/Main1.java
@@ -1,1 +1,19 @@
 
+// Multiple Inheritance
+interface A {
+    void show();
+}
+
+interface B {
+    void display();
+}
+
+class Test implements A, B {
+    public void show() {
+        System.out.println("Show method");
+    }
+
+    public void display() {
+        System.out.println("Display method");
+    }
+}
