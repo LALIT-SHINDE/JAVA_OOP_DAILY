094cf1de3ee1f11c696631ce1ce2c470701c42c7
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-28 21:52:10 +0530

    Update Main.java

--- Day07-Interfaces/Main.java
+++ Day07-Interfaces/Main.java
@@ -1,1 +1,32 @@
 
+// Interfaces
+interface Payment {
+    void pay(double amount);
+}
+
+class CreditCard implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card");
+    }
+}
+
+class UPI implements Payment {
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using UPI");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Payment p;
+
+        p = new CreditCard();
+        p.pay(1000);
+
+        p = new UPI();
+        p.pay(500);
+    }
+}
+// output:
+// Paid 1000.0 using Credit Card
+// Paid 500.0 using UPI
