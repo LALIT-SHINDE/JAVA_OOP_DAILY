f7890e86f35ff33238e8047262de7a0fc51379a6
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-04-28 20:51:03 +0530

    Update Main1.java

--- Day07-Abstraction/Main1.java
+++ Day07-Abstraction/Main1.java
@@ -1,1 +1,21 @@
+// 2 Interface
+interface Animal {
+    void sound();
+}
 
+class Cat implements Animal {
+    public void sound() {
+        System.out.println("Cat meows");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Cat c = new Cat();
+        c.sound();
+    }
+}
+/*
+Output:
+Cat meows
+*/
