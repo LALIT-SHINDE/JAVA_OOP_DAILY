477476befe7fc75b976fc7dfca1503e455d5b95f
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-05 22:22:46 +0530

    Update Main.java

--- Day08:Multilevel-Inheritance/Main.java
+++ Day08:Multilevel-Inheritance/Main.java
@@ -1,1 +1,25 @@
+package oop;
 
+class Manager extends Employee {
+    protected double salary;
+
+    public Manager(String name, String dob, String empId, double salary) {
+        super(name, dob, empId);
+        this.salary = salary;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Salary: ₹" + salary);
+    }
+}
+
+// SalesManager.java (Leaf Class)
+class SalesManager extends Manager {
+    private double commission;
+
+    public SalesManager(String name, String dob, String empId, double salary, double commission) {
+        super(name, dob, empId, salary);
+        this.commission = commission;
+    }
