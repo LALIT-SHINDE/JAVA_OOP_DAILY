7fee9eed4e96f1e09b07f8c85552fbbd57dfc956
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-07 22:57:08 +0530

    Update Main1.java

--- Day10:Hybrid-Inheritance/Main1.java
+++ Day10:Hybrid-Inheritance/Main1.java
@@ -39,3 +39,93 @@ class HybridInheritance{
         b.bike();
     }
 }
+/* 
+Hybrid Inheritance in Java
+
+Description  
+This Java program demonstrates the concept of Hybrid Inheritance using classes:
+- Vehicle
+- Car
+- Bike
+- SportsCar
+
+The program combines:
+- Hierarchical Inheritance
+- Multilevel Inheritance
+
+---
+
+Concepts Used
+- Class and Object
+- Inheritance
+- Hierarchical Inheritance
+- Multilevel Inheritance
+- Hybrid Inheritance
+- Method Calling
+
+---
+
+Inheritance Structure
+
+        Vehicle
+       /       \
+     Car       Bike
+      ↓
+  SportsCar
+
+---
+
+Class Details
+
+Vehicle  
+Contains:
+- vehicle() method
+
+Car  
+Inherits from Vehicle  
+Contains:
+- car() method
+
+Bike  
+Inherits from Vehicle  
+Contains:
+- bike() method
+
+SportsCar  
+Inherits from Car  
+Contains:
+- sportsCar() method
+
+---
+
+Features
+- Demonstrates hybrid inheritance
+- Shows parent-child class relationships
+- Demonstrates method inheritance
+- Uses objects to access inherited methods
+- Demonstrates code reusability
+
+---
+
+Sample Output
+
+This is Vehicle Class
+This is Car Class
+This is Sports Car Class
+
+This is Vehicle Class
+This is Bike Class
+
+---
+
+Types of Inheritance Used
+
+Hierarchical Inheritance:
+Vehicle → Car
+Vehicle → Bike
+
+Multilevel Inheritance:
+Vehicle → Car → SportsCar
+
+Combination of both forms Hybrid Inheritance. 
+*/
