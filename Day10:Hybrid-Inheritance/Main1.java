e885f25a6ef8e27532455ffc33a5e59e12b422d9
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-07 22:54:29 +0530

    Update Main1.java

--- Day10:Hybrid-Inheritance/Main1.java
+++ Day10:Hybrid-Inheritance/Main1.java
@@ -1,1 +1,41 @@
+class Vehicle{
+    void vehicle(){
+        System.out.println("This is Vehicle Class");
+    }
+}
 
+class Car extends Vehicle{
+    void car(){
+        System.out.println("This is Car Class");
+    }
+}
+
+class Bike extends Vehicle{
+    void bike(){
+        System.out.println("This is Bike Class");
+    }
+}
+
+class SportsCar extends Car{
+    void sportsCar(){
+        System.out.println("This is Sports Car Class");
+    }
+}
+
+class HybridInheritance{
+    public static void main(String args[]){
+
+        SportsCar s = new SportsCar();
+
+        s.vehicle();
+        s.car();
+        s.sportsCar();
+
+        System.out.println();
+
+        Bike b = new Bike();
+
+        b.vehicle();
+        b.bike();
+    }
+}
