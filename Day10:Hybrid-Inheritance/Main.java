0be670baadb64a66ef07078080e852b1ff14ec2b
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-07 22:43:29 +0530

    Update Main.java

--- Day10:Hybrid-Inheritance/Main.java
+++ Day10:Hybrid-Inheritance/Main.java
@@ -1,1 +1,42 @@
+// Hybrid Inheritance
+class Animal{
+    void animal(){
+        System.out.println("This is Animal Class");
+    }
+}
 
+class Dog extends Animal{
+    void dog(){
+        System.out.println("This is Dog Class");
+    }
+}
+
+class Cat extends Animal{
+    void cat(){
+        System.out.println("This is Cat Class");
+    }
+}
+
+class Puppy extends Dog{
+    void puppy(){
+        System.out.println("This is Puppy Class");
+    }
+}
+
+class HybridInheritance{
+    public static void main(String args[]){
+
+        Puppy p = new Puppy();
+
+        p.animal();
+        p.dog();
+        p.puppy();
+
+        System.out.println();
+
+        Cat c = new Cat();
+
+        c.animal();
+        c.cat();
+    }
+}
