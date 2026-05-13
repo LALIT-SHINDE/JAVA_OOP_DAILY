d866804660e3e054bb6ef69852ea538d5cb23085
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-13 21:14:32 +0530

    Update Client.java

--- Day15:TCP_Echo/Client.java
+++ Day15:TCP_Echo/Client.java
@@ -1,1 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) {
+        try {
+            // Connect to server
+            Socket s = new Socket("localhost", 5000);
+
+            // Input and Output streams
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            Scanner sc = new Scanner(System.in);
+
+            // Take message from user
+            System.out.print("Enter message: ");
+            String msg = sc.nextLine();
+
+            // Send message to server
+            dos.writeUTF(msg);
+
+            // Receive echo from server
+            String response = dis.readUTF();
+
+            System.out.println("Echo from server: " + response);
+
+            // Close connections
+            dis.close();
+            dos.close();
+            s.close();
+            sc.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
