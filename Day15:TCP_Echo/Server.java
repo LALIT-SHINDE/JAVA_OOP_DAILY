0f1cbf5c6b18317b2066727d3f000b462eb274cd
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-13 21:11:06 +0530

    Update Server.java

--- Day15:TCP_Echo/Server.java
+++ Day15:TCP_Echo/Server.java
@@ -1,1 +1,36 @@
+import java.io.*;
+import java.net.*;
 
+public class Server {
+    public static void main(String[] args) {
+        try {
+           
+            ServerSocket ss = new ServerSocket(5000);
+
+            System.out.println("Server is waiting for client...");
+            
+            Socket s = ss.accept();
+          
+            System.out.println("Client connected");
+           
+            DataInputStream dis = new DataInputStream(s.getInputStream());
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+            String msg = dis.readUTF();
+
+            System.out.println("Client says: " + msg);
+
+            dos.writeUTF(msg);
+
+            System.out.println("Message echoed back");
+
+            dis.close();
+            dos.close();
+            s.close();
+            ss.close();
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
