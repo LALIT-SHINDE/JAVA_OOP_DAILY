9206a75af26aa43acf068b7a1226ffc8ee526fd8
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-15 20:24:26 +0530

    Update GetPostDemo.java

--- Day17:GetPostServletDemo/GetPostDemo.java
+++ Day17:GetPostServletDemo/GetPostDemo.java
@@ -1,1 +1,34 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
+public class GetPostDemo extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String name = req.getParameter("name");
+
+        out.println("<html><body>");
+        out.println("<h2>GET Method Example</h2>");
+        out.println("<h3>Hello " + name + "</h3>");
+        out.println("</body></html>");
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+
+        String name = req.getParameter("name");
+
+        out.println("<html><body>");
+        out.println("<h2>POST Method Example</h2>");
+        out.println("<h3>Welcome " + name + "</h3>");
+        out.println("</body></html>");
+    }
+}
