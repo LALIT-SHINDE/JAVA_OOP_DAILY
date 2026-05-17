516113090aaa6eb28f313ef623c9c33401d9a63d
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-17 20:13:30 +0530

    Implement TaskController for async task processing

--- Day19:AsyncProcess/TaskController.java
+++ Day19:AsyncProcess/TaskController.java
@@ -1,1 +1,21 @@
+package com.example.asyncprocess.controller;
 
+import com.example.asyncprocess.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskController {
+
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping("/start")
+    public String startTask() {
+
+        taskService.processTask();
+
+        return "Task is processing asynchronously...";
+    }
+}
