cb12589282a95df745a43c3ca803ff57053c0769
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-17 20:12:03 +0530

    Update TaskService.java

--- Day19:AsyncProcess/TaskService.java
+++ Day19:AsyncProcess/TaskService.java
@@ -1,1 +1,23 @@
+package com.example.asyncprocess.service;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    @Async
+    public void processTask() {
+
+        try {
+            System.out.println("Task Started: " + Thread.currentThread().getName());
+
+            Thread.sleep(5000);
+
+            System.out.println("Task Completed: " + Thread.currentThread().getName());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
