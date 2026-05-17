e3ee5ad4471823cc53153becfe44b3b9887c2974
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-05-17 20:11:14 +0530

    Add Spring Boot and async support to application

--- Day19:AsyncProcess/AsyncProcessApplication.java
+++ Day19:AsyncProcess/AsyncProcessApplication.java
@@ -1,1 +1,14 @@
+package com.example.asyncprocess;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class AsyncProcessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncProcessApplication.class, args);
+    }
+}
