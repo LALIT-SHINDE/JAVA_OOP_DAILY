f644f4216b7a65fac1a37eab52f04da849345c64
LALIT-SHINDE <lalitshinde8032@gmail.com>
2026-06-03 14:23:45 +0530

    Update Main2.java

--- Day06-Abstraction/Main2.java
+++ Day06-Abstraction/Main2.java
@@ -1,1 +1,68 @@
+abstract class Animal {
 
+    abstract void sound();
+
+    void sleep() {
+        System.out.println("Animal is sleeping");
+    }
+}
+
+class Dog extends Animal {
+
+    void sound() {
+        System.out.println("Dog barks");
+    }
+
+    void run() {
+        System.out.println("Dog is running");
+    }
+}
+
+class Cat extends Animal {
+
+    void sound() {
+        System.out.println("Cat meows");
+    }
+
+    void jump() {
+        System.out.println("Cat is jumping");
+    }
+}
+
+class Cow extends Animal {
+
+    void sound() {
+        System.out.println("Cow moos");
+    }
+
+    void eat() {
+        System.out.println("Cow is eating grass");
+    }
+}
+
+public class Main2{
+
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+        d.sound();
+        d.sleep();
+        d.run();
+
+        System.out.println();
+
+        Cat c = new Cat();
+        c.sound();
+        c.sleep();
+        c.jump();
+
+        System.out.println();
+
+        Cow cw = new Cow();
+        cw.sound();
+        cw.sleep();
+        cw.eat();
+    }
+}
+/*
+*/
